e7845fa7c5f25263e507b9dfa11590958f6fa3f1
RuslanPro01 <ruslan91999@gmail.com>
2023-05-11 11:07:40 +0300

    feat: Добавит вспомогательные функции и тип для моковых данных

---
+++ src/common/utils.ts
@@ -0,0 +1,16 @@
+export function getRandomValue(min: number, max: number): number {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+export function getRandomItems<T>(items: T[]): T[] {
+  const start = getRandomValue(0, items.length - 1);
+  const end = getRandomValue(start, items.length - 1);
+
+  return items.slice(start, end);
+}
+
+export function getRandomItem<T>(items: T[]): T {
+  const index = getRandomValue(0, items.length - 1);
+
+  return items[index];
+}
---
+++ src/types/mock-data.ts
@@ -0,0 +1,19 @@
+export type MockData = {
+  title: string[];
+  description: string[];
+  date: string[];
+  city: string[];
+  previewUrl: string[];
+  photoUrls: string[];
+  isPremium: boolean[];
+  isFavorite: boolean[];
+  rating: number[];
+  propertyType: string[];
+  numberOfBedrooms: number[];
+  numberOfGuests: number[];
+  pricePerNight: number[];
+  amenities: string[];
+  userId: number[];
+  commentsCounter: number[];
+  coordinates: string[];
+}
