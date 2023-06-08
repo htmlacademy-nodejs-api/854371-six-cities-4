8dbf74adfa0f9b05a7cd31c4d48d8c4c492c0f9e
Ruslan Shevchenko <ruslan91999@gmail.com>
2023-06-08 17:10:17 +0300

    fix ДЗ2: Исправит генерацию пользователей
    Раньше в базе создавался только один пользователь, теперь пользователей создаётся до 19

--- src/common/user.ts
+++ src/common/user.ts
@@ -32,16 +32,15 @@ function createUserAvatar(): string {
   return `https://api.dicebear.com/6.x/avataaars/jpg?seed=${Math.round(Math.random() * 100000)}`;
 }
 
-const autoUser: AutoUserType = {
-  password: 'pass123456',
-  avatar: createUserAvatar(),
-  userType: 'base',
-  email: getRandomEmail(),
-  name: getRandomEmail().split('@')[0]
-
-};
-
 export function createUser() {
+  const autoUser: AutoUserType = {
+    password: 'pass123456',
+    avatar: createUserAvatar(),
+    userType: 'base',
+    email: getRandomEmail(),
+    name: getRandomEmail().split('@')[0]
+  };
+
   return ({
     name: autoUser.name,
     email: autoUser.email,
